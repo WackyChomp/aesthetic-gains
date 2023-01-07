07ad73acbe2315af67dfb86844a3b17f7b252294
Wilson Chan <wilsonchan54@gmail.com>
2023-01-07 17:15:00 -0500

    Created ExerciseDetail page to fetch data from exercisedb and youtube api to display exercise info and videos (WIP)

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchAPIData';
+import Detail from '../components/Detail';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();     // exercise associated to id number
+
+  useEffect(() => {       // populates the useState
+    const fetchExerciseData = async () => {
+      const exerciseDBUrl = 'https://exercisedb.p.rapidapi.com/';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseDBUrl}/exercises/${id}`, exerciseOptions)
+      setExerciseDetail(exerciseDetailData);
+    }
+
+    fetchExerciseData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+    </Box>
   )
 }
 
